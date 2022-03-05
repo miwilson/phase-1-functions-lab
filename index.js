fab26e4c38a882b37b436e0b68b0b856b7d2c5b2
Michaela Wilson <+>
2022-03-05 13:31:33 -0700

    commit tree 20c735c7cf2a28793bd6e8ecc7968667022e6252

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(number){
+    return Math.abs(42 - number);
+}
+
+function distanceFromHqInFeet(number){
+    return distanceFromHqInBlocks(number) * 264;
+}
+function distanceTravelledInFeet(start, destination){
+    return (Math.abs(start - destination)) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    if (((Math.abs(start - destination)) * 264) <= 400){
+        return 0;
+    }
+    else if ((((Math.abs(start - destination))* 264) > 400) && (((Math.abs(start -destination))* 264)<= 2000)){
+        return (((((Math.abs(start - destination)) *264) - 400) * 0.02));
+    }
+    else if ((((Math.abs(start -destination))* 264) > 2000) && (((Math.abs(start -destination))* 264) < 2500)){
+        return 25;
+    }
+    else if (((Math.abs(start -destination))* 264) >= 2500){
+        return `cannot travel that far`;
+    }
+}
